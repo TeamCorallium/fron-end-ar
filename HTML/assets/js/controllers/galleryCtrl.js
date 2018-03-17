b4a419983878e157aa8a8e7ae65510300bd8fc47
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-03-17 13:06:58 +0800

    adding aframe

--- HTML/assets/js/controllers/galleryCtrl.js
+++ HTML/assets/js/controllers/galleryCtrl.js
@@ -1,5 +1,10 @@
 app.controller('GalleryCtrl', ["$scope", "$state", "$cookies", "$window",
     function($scope, $state, $cookies, $window) {
+        
+        $scope.objects3d = [];
 
+        $scope.goToObject = function() {
+            $state.go('object3d');
+        };
     }
 ]);
